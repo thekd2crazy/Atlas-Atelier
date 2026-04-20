666a528bbc510ad1105b016a980c1617f7a228ba
thekd2crazy <23230@ecam.be>
2026-04-20 19:05:15 +0200

    Creation d'un fichier qui a tous les types (classes) utilisees dans le code

---
+++ frontend/types/type-composant.tsx
@@ -0,0 +1,37 @@
+
+// Creer un composant : 
+export type ComposantCreate = {
+  nom: string;
+  reference: string;
+  categorie: string;
+  prix: number;
+  emplacement: string;
+  quantite: number;
+  photo_url: string;
+};
+
+// Utiliser dans getAllcomposants()
+export type Composant = {
+    id : number
+    nom : string
+    categorie : string
+    reference : string 
+    emplacement : string 
+    quantite : number | string
+    prix : number | string
+    photo_url : string 
+};
+
+// Utiliser pour UpdateComposant()
+export type ComposantUpdate = {
+    nom : string 
+    categorie : string
+    reference : string 
+    emplacement : string 
+    quantite : number | string 
+    prix : number | string 
+    photo_url : string 
+};
+
+
+
