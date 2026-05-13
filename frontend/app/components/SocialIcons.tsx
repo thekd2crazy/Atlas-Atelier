b42667ad74872b20324ad2a407e9c32665f7a7bd
Mike-oss-star <23390@ecam.be>
2026-05-13 13:16:45 +0200

    Navbar

--- frontend/app/components/SocialIcons.tsx
+++ frontend/app/components/SocialIcons.tsx
@@ -4,14 +4,14 @@ export default function SocialIcons() {
     return (
 
         <li className="mx-3 flex items-center p-3">
-            <a href="#" target="_blank" className="mx-3  transition-colors duration-200 hover:scale-125 hover:text-orange-500 ">
+            <a href="#" target="_blank" className="mx-3  transition-colors duration-200 hover:scale-125 hover:text-green-200 ">
                 < FaInstagram size={30} />
             </a>
-            <a href="#" target="_blank" className="mx-3 transition-colors duration-200 hover:scale-125 hover:text-orange-500">
+            <a href="#" target="_blank" className="mx-3 transition-colors duration-200 hover:scale-125 hover:text-green-200">
                 <FaSnapchat size={30} />
             </a>
 
-            <a href="#" target="_blank" className="mx-3  transition-colors duration-200 hover:scale-125 hover:text-orange-500">
+            <a href="#" target="_blank" className="mx-3  transition-colors duration-200 hover:scale-125 hover:text-green-200">
                 <FaTelegram size={30} />
             </a>
         </li>
